12d2bde1ba8ee5cb0f7e1eb8295c70a9a34d7f9e
Nicolas Kuyumciyan <nkuyumciyan@medallia.com>
2018-05-28 12:46:40 -0300

    Update readme with requirements

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # cripto-tpe
 Trabajo Práctico Especial de Criptografía y Seguridad | ITBA 2018
+
+### Requirements
+
+Must have JCE (Java Cryptography Extension) installed.
+
+Download [here](http://www.oracle.com/technetwork/java/javase/downloads/jce8-download-2133166.html) (Java 8)
+
+Instructions on installations for Windows/Linux are on downloaded zip file ReadMe.
+For Mac OS see [steps](https://stackoverflow.com/questions/37741142/how-to-install-unlimited-strength-jce-for-java-8-in-os-x)
--- src/main/java/ar/edu/itba/crypto/TestMain.java
+++ src/main/java/ar/edu/itba/crypto/TestMain.java
@@ -1,20 +1,20 @@
 package ar.edu.itba.crypto;
 
-import ar.edu.itba.crypto.algorithms.AES128;
-import ar.edu.itba.crypto.algorithms.AES256;
-import ar.edu.itba.crypto.algorithms.EncryptionAlgorithm;
-import ar.edu.itba.crypto.algorithms.EncryptionMode;
+import ar.edu.itba.crypto.algorithms.*;
+
+import java.util.Base64;
 
 public class TestMain {
 
 	public static void main(String[] args) {
-		String message = "Super secret message";
+		String message = "e";
 		String pass = "Random Password!!";
 		System.out.println("Message: " + message);
-		EncryptionAlgorithm encryptionAlgorithm = new AES128();
-		byte[] cipheredText = encryptionAlgorithm.encrypt(message.getBytes(), EncryptionMode.OFB, pass.getBytes());
-		System.out.println("Ciphered: " + new String(cipheredText));
-		byte[] decryptedText = encryptionAlgorithm.decrypt(cipheredText, EncryptionMode.OFB, pass.getBytes());
+		EncryptionAlgorithm encryptionAlgorithm = new AES192();
+		EncryptionMode mode = EncryptionMode.CBC;
+		byte[] cipheredText = encryptionAlgorithm.encrypt(message.getBytes(), mode, pass.getBytes());
+		System.out.println("Ciphered: " + new String(Base64.getEncoder().encode(cipheredText)));
+		byte[] decryptedText = encryptionAlgorithm.decrypt(cipheredText, mode, pass.getBytes());
 		System.out.println("Deciphered: " + new String(decryptedText));
 	}
 }
